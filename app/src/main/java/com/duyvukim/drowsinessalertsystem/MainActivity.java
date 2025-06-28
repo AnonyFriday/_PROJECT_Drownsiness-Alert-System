f7b3817715b76b7e3b989722d831c3a46d3b4731
plozdev <azure17171@gmail.com>
2025-06-28 14:04:37 +0700

    adds main activity

---
+++ app/src/main/java/com/duyvukim/drowsinessalertsystem/MainActivity.java
@@ -0,0 +1,50 @@
+package com.duyvukim.drowsinessalertsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.duyvukim.drowsinessalertsystem.camera.CameraActivity;
+import com.duyvukim.drowsinessalertsystem.databinding.ActivityMainBinding;
+import com.duyvukim.drowsinessalertsystem.utils.AppCts;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private TextInputEditText examCodeInput;
+    private MaterialButton joinExamButton;
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initializeViews();
+        joinExamButton.setOnClickListener(v->{
+            String code;
+            if (examCodeInput.getText()!=null)
+                code = examCodeInput.getText().toString().trim();
+            else {
+                examCodeInput.setError("Please enter code!");
+                return ;
+            }
+
+            if (code.equals(AppCts.Notifications.EXAM_CODE))
+                startActivity(new Intent(MainActivity.this, CameraActivity.class));
+            else examCodeInput.setError("Code isn't found");
+        });
+    }
+
+    private void initializeViews() {
+        joinExamButton = binding.joinExamButton;
+        examCodeInput = binding.examCodeInput;
+    }
+}
